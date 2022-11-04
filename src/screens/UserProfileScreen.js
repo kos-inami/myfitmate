e7ef7d96fa3a8b8a64a4773b3d4b1a55da959b6b
Kosuke Inami <kosuke.i.619@gmail.com>
2022-11-04 22:09:48 +1100

    upload image to firebase in progress

--- src/screens/UserProfileScreen.js
+++ src/screens/UserProfileScreen.js
@@ -10,8 +10,7 @@ import * as ImagePicker from 'expo-image-picker'
 // Firebase config ---------- // installed package
 import { firebaseConfig } from '../../config/Config'
 import { initializeApp } from 'firebase/app'
-import { getStorage, ref, uploadBytes } from "firebase/storage";
-// import * as firebaseIs from "firebase";
+import { getStorage, ref, uploadBytes, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 
 const app = initializeApp( firebaseConfig ) // initialize Firebase app and store ref in a variable
 const storage = getStorage(app)
@@ -252,13 +251,55 @@ export default function UserProfileScreen( props ) {
 
         // START: upload a image file to firebase -----------------------------------------------------
 
+        const metadata = {
+            contentType: 'image/jpeg',
+        };
+
+        // Upload file and metadata to the object 'images/mountains.jpg'
         const storageRef = ref(storage, 'userImages/' + imageName);
-        // 'file' comes from the Blob or File API
-        uploadBytes(storageRef, image)
-            .then((snapshot) => {
-                console.log('Uploaded a blob or file!')
-                console.log(snapshot)
+        const uploadTask = uploadBytesResumable(storageRef, image, metadata);
+
+        // Listen for state changes, errors, and completion of the upload.
+        uploadTask.on('state_changed',
+        (snapshot) => {
+            // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+            case 'paused':
+                console.log('Upload is paused');
+                break;
+            case 'running':
+                console.log('Upload is running');
+                break;
+            }
+        }, 
+        (error) => {
+            // A full list of error codes is available at
+            // https://firebase.google.com/docs/storage/web/handle-errors
+            switch (error.code) {
+            case 'storage/unauthorized':
+                // User doesn't have permission to access the object
+                break;
+            case 'storage/canceled':
+                // User canceled the upload
+                break;
+
+            // ...
+
+            case 'storage/unknown':
+                // Unknown error occurred, inspect error.serverResponse
+                break;
+            }
+        }, 
+        () => {
+            // Upload completed successfully, now we can get the download URL
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
             });
+        }
+        );
+
         // END: upload a image file to firebase -----------------------------------------------------
 
         // navigation.reset( {index: 0, routes: [{name: "UserSettingScreen"}]})
